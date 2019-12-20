ea0f72583bbc94b90a80f0c4cab6b4c501f1ee35
German Goldenstein <german.goldenstein@gmail.com>
2019-12-20 11:39:19 -0300

    Add tests as a separated sln

---
+++ BasicVideoChat/BasicVideoChat.Tests/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+[TestClass]
+public class MathTests
+{
+    [TestMethod]
+    public void Test_Add()
+    {
+        var actual = MathHelper.Add(1, 2);
+        var expected = 2;
+        Assert.AreEqual(expected, actual);
+    }
+}
+public class MathHelper
+{
+    public static int Add(int a, int b) => a + b;
+}
