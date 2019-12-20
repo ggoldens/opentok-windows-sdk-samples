47d5acd748d0a9c610118ffe033044527ef693ae
German Goldenstein <german.goldenstein@gmail.com>
2019-12-20 12:46:21 -0300

    update deps

--- BasicVideoChat/BasicVideoChat.Tests/UnitTestProject1/UnitTest1.cs
+++ BasicVideoChat/BasicVideoChat.Tests/UnitTestProject1/UnitTest1.cs
@@ -5,7 +5,7 @@ public class MathTests
     [TestMethod]
     public void Test_Add()
     {
-        var actual = MathHelper.Add(1, 2);
+        var actual = MathHelper.Add(1, 1);
         var expected = 2;
         Assert.AreEqual(expected, actual);
     }
